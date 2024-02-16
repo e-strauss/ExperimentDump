c11322ed1396d96684fbdd072caae954b24bb084
e-strauss <92718421+e-strauss@users.noreply.github.com>
2024-02-16 16:28:09 +0100

    approx for curve in R^2

--- src/approx-curve-alpha-sequence.py
+++ src/approx-curve-alpha-sequence.py
@@ -8,24 +8,21 @@ def orthogonal_point(p1, p2):
 
 
 def approx(arg_x, arg_dx, step=0.05):
-    #plt.scatter(arg_x[:, 0], arg_x[:, 1], marker='x', s=50)
+    ax1.scatter(arg_x[:, 0], arg_x[:, 1], marker='x', s=50)
     for i in range(len(arg_x) - 1):
-        x3 = orthogonal_point(arg_x[i], arg_x[i + 1])
         b = (arg_x[i + 1] - arg_x[i]) / 2
         a = (b - arg_dx) / 2
         c = arg_x[i] + b - a
-        # c = x3
-        # a = (argx_[i] + argx_[i + 1] - 2 * x3) / 2
 
         alpha = np.arange(-1, 1 + step, step).reshape((-1, 1))
         points = a * alpha ** 2 + b * alpha + c
-        plt.scatter(points[:, 0], points[:, 1], marker='x', s=3, c='g')
+        ax1.scatter(points[:, 0], points[:, 1], marker='x', s=3, c='g')
         arg_dx = (2 * a + b)
         arg_dx = arg_dx / np.linalg.norm(arg_dx) * np.linalg.norm(arg_x[i + 1] - arg_x[i]) / 4
 
 
 def approx2(arg_x, arg_dx, arg_dx2, step=0.05):
-    plt.scatter(arg_x[:, 0], arg_x[:, 1], marker='x', s=50)
+    ax2.scatter(arg_x[:, 0], arg_x[:, 1], marker='x', s=50)
     for i in range(len(arg_x) - 1):
         a = (arg_x[i + 1] - arg_x[i] - 2 * arg_dx - 2 * arg_dx2) / 8
         b = (6 * a + arg_dx2) / 2
@@ -34,14 +31,16 @@ def approx2(arg_x, arg_dx, arg_dx2, step=0.05):
 
         alpha = np.arange(-1, 1 + step, step).reshape((-1, 1))
         points = a * alpha ** 3 + b * alpha ** 2 + c * alpha + d
-        plt.scatter(points[:, 0], points[:, 1], marker='x', s=1, c='r')
-        arg_dx = 3 * a + 2*b + c
+        ax2.scatter(points[:, 0], points[:, 1], marker='x', s=1, c='r')
+        arg_dx = 3 * a + 2 * b + c
         arg_dx = arg_dx / np.linalg.norm(arg_dx) * np.linalg.norm(arg_x[i + 1] - arg_x[i]) / 4
         arg_dx2 = 6 * a + 2 * b
         arg_dx2 = arg_dx2 / np.linalg.norm(arg_dx2) * np.linalg.norm(arg_x[i + 1] - arg_x[i]) / 8
 
 
-step_size = 0.4
+f, (ax1, ax2) = plt.subplots(2, 1, figsize=(6, 10))
+
+step_size = 0.6
 x = np.linspace(2, 5, 5).reshape((-1, 1))
 x = np.hstack((x, x + 1))
 dx = np.array([1, 1])
@@ -55,16 +54,16 @@ x = np.arange(0, 2 * np.pi, step_size).reshape((-1, 1))
 a_ = np.sin(x) + 2.5
 b_ = np.cos(x) + 5.5
 x = np.hstack((a_, b_))
-dx = np.array([1, -0.1]) / 30
-dx2 = np.array([1, -0.05]) / 30
+dx = np.array([1, -0.1])/6
+dx2 = np.array([0, 0])
 approx2(x, dx, dx2)
-#approx(x, dx)
+approx(x, dx)
 
 x1 = x[0]
 x2 = x[1]
 x = np.vstack((x1, x2, orthogonal_point(x1, x2)))
 # plt.scatter(x[:, 0], x[:, 1], marker='x', s=50)
 
-#plt.xlim(2, 4)
-#plt.ylim(5, 7)
+# plt.xlim(2, 4)
+# plt.ylim(5, 7)
 plt.show()
